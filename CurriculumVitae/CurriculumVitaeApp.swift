6cec80b279a167f8e5f3b576642b510778840f7f
Cindoushka <cindie-baj@live.fr>
2022-07-04 12:47:19 +0200

    modifications

--- CurriculumVitae/CurriculumVitaeApp.swift
+++ CurriculumVitae/CurriculumVitaeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CurriculumVitaeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Profile()
         }
     }
 }
--- CurriculumVitae/CustomTabView.swift
+++ CurriculumVitae/CustomTabView.swift
@@ -1,38 +1,38 @@
+////
+////  CustomTabView.swift
+////  CurriculumVitae
+////
+////  Created by apprenant50 on 12/06/2022.
+////
 //
-//  CustomTabView.swift
-//  CurriculumVitae
+//import SwiftUI
 //
-//  Created by apprenant50 on 12/06/2022.
-//
-
-import SwiftUI
-
-struct CustomTabView<Content: View>: View {
-    
-    let tabs: [TabItemData]
-    @Binding var selectedIndex: Int
-    @ViewBuilder let content: (Int) -> Content
-    
-    
-    var body: some View {
-        ZStack {
-            TabView(selection: $selectedIndex) {
-                ForEach(tabs.indices) { index in
-                    content(index)
-                        .tag(index)
-                }
-            }
-            
-            VStack {
-                Spacer()
-                TabBottomView(tabBarItem: tabs, selectedIndex: $selectedIndex)
-            }
-            .padding(.bottom, 8)
-        }
-    }
-}
-struct CustomTabView_Previews: PreviewProvider {
-    static var previews: some View {
-        CustomTabView(tabs: tabBarItem, selectedIndex: .constant(0), content: <#T##(Int) -> Content#>)
-    }
-}
+//struct CustomTabView<Content: View>: View {
+//    
+//    let tabs: [TabItemData]
+//    @Binding var selectedIndex: Int
+//    @ViewBuilder let content: (Int) -> Content
+//    
+//    
+//    var body: some View {
+//        ZStack {
+//            TabView(selection: $selectedIndex) {
+//                ForEach(tabs.indices) { index in
+//                    content(index)
+//                        .tag(index)
+//                }
+//            }
+//            
+//            VStack {
+//                Spacer()
+//                TabBottomView(tabBarItem: tabs, selectedIndex: $selectedIndex)
+//            }
+//            .padding(.bottom, 8)
+//        }
+//    }
+//}
+//struct CustomTabView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CustomTabView(tabs: tabBarItem, selectedIndex: .constant(0), content: <#T##(Int) -> Content#>)
+//    }
+//}
